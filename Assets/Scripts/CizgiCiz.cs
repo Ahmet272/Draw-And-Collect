5a686048627e2a76a2f3d02f10916d290ed6390b
Ahmet Altunbaş <140913760+Ahmet272@users.noreply.github.com>
2024-07-22 06:10:32 +0300

    aa

---
+++ Assets/Scripts/CizgiCiz.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CizgiCiz : MonoBehaviour
+{
+    public GameObject LinePrefab;
+    public GameObject Cizgi;
+
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D EdgeCollider2D;
+    void Update()
+    {
+        
+    }
+}
