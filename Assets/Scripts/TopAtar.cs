960618553f584ff66c62844d5cbb421a09c2e797
Ahmet Altunbaş <140913760+Ahmet272@users.noreply.github.com>
2024-07-22 08:53:28 +0300

    ss

--- Assets/Scripts/TopAtar.cs
+++ Assets/Scripts/TopAtar.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class TopAtar : MonoBehaviour
 {
     [SerializeField] private GameObject[] Toplar;
+    [SerializeField] private GameObject TopAtarMerkezi;
 
     int AktifTopIndex;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
-        { 
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Toplar[AktifTopIndex].transform.position = TopAtarMerkezi.transform.position;
+            Toplar[AktifTopIndex].SetActive(true);
+
+            float aci = Random.Range(70f, 110f);
+            Vector3 Pos = Quaternion.AngleAxis(aci,Vector3.forward)* Vector3.right;
+            Toplar[AktifTopIndex].gameObject.GetComponent<Rigidbody2D>().AddForce(Pos * 800);
+
+
+            if (AktifTopIndex != Toplar.Length - 1)
+            {
+                AktifTopIndex++;
+            }
+            else { AktifTopIndex = 0; }
+
+
         }
 
     }
